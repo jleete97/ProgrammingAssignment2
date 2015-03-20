52badf93a182b440ba50a8ccab7d7016eef09f66
Jon Leete <jleete97@gmail.com>
2015-03-20 18:54:35 -0400

    initial cut on functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    
+    list(set = set,
+         get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    
+    if (is.null(s)) {
+        m <- x$get()
+        s <- solve(m, ...)
+        x$setsolve(s)
+    }
+    
+    s
 }
